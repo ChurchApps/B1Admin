38b6d74b59c2c9f5ef59ae74afdea949946dcb3a
Jeremy Zongker <jeremy@zongker.net>
2026-01-08 23:08:58 -0600

    Fixed build error

--- src/serving/tasks/automations/components/ConditionSelect.tsx
+++ src/serving/tasks/automations/components/ConditionSelect.tsx
@@ -1,6 +1,6 @@
 import { FormControl, InputLabel, MenuItem, Select, type SelectChangeEvent, Stack } from "@mui/material";
 import React from "react";
-import { ConditionHelper } from "../components";
+import { ConditionHelper } from "../../../../helpers";
 import { type ConditionInterface, Locale } from "@churchapps/apphelper";
 
 interface Props {
